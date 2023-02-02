913c0dbefe1e34fa1c8a5cae614f148e26c3136e
amira <amira@AMIRA>
2023-02-02 23:23:14 +0000

    Add project files.

---
+++ Problem4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void starPrint(int num)
+            {
+                int empty = num - 1;
+
+                for (int i = 0; i < num; i++)
+                {
+                    for (int a = 0; a < empty; a++)
+                        Console.Write(" ");
+                    for (int a = 0; a <= i; a++)
+                        Console.Write("* ");
+
+                    Console.Write("\n");
+                    empty--;
+                }
+                empty = 0;
+
+                for (int i = num; i > 0; i--)
+                {
+                  
+                    for (int a = 0; a < empty; a++)
+                        Console.Write(" ");
+                    for (int a= 0; a< i; a++)
+                        Console.Write("* ");
+
+                    Console.Write("\n");
+                    empty++;
+                }
+            }
+
+            // we call the function starPrint
+            starPrint(5);
+
+            
+        }
+    }
+    }
+
